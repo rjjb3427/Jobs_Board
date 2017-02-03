0cc3b58add110c8ce7ac9fbe23c78304330f4d2b
Roderick <notmine@rjbradley.local>
2017-02-03 07:43:14 -0600

    Added a category model and added categories to it via console

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202161150) do
+ActiveRecord::Schema.define(version: 20170203133506) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
